34e389bc20497425c53f8c8f48907087e4683cd2
Magnus <fluhay@gmail.com>
2020-01-16 09:13:39 +0100

    test

---
+++ BotApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BotApp
+//
+//  Created by Magnus Ahlqvist on 2020-01-10.
+//  Copyright © 2020 Magnus Ahlqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var inputTeamName: UILabel!
+    
+    
+    @IBAction func buttonSave(_ sender: UIButton) {
+    }
+    
+}
+
